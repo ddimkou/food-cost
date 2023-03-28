e41cffd30d97555a78d7ea67bb9b0ad453e1551e
ddimkou <ddimkou@outlook.com>
2023-03-28 13:39:35 +0300

    fixed calculator
    fixed the event listener of calculator on trial.html

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("hello world");
-
   // Price/kg generator
   const amountIngredient = document.querySelector("#amount-of-ingredient");
   const priceIngredient = document.querySelector("#price-of-ingredient");
@@ -20,46 +18,34 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Food cost calculator
-  // Get all the input elements
+
   const ingredientNames = document.querySelectorAll(".ingredient-name");
   const pricePerKgs = document.querySelectorAll(".price-per-kg");
   const amountsUsed = document.querySelectorAll(".amount-used");
   const sellingPriceInput = document.getElementById("selling-price");
-  const calculateButton = document.getElementById("btn-calculate");
+  const calculateButton = document.querySelector("#btn-calculate");
   const resultElement = document.getElementById("result");
 
-  // Add event listener to the calculate button
   calculateButton.addEventListener("click", calculateFoodCost);
 
-  // Calculate the food cost when the calculate button is clicked
   function calculateFoodCost() {
-    // Initialize the total cost to zero
     let totalCost = 0;
 
-    // Loop through all the input elements and calculate the cost for each ingredient
+    // loop inputs to get the cost for every ingredient
     for (let i = 0; i < ingredientNames.length; i++) {
       const ingredientName = ingredientNames[i].value;
       const pricePerKg = pricePerKgs[i].value;
       const amountUsed = amountsUsed[i].value;
 
-      // Calculate the cost for this ingredient and add it to the total cost
       const costForIngredient = (pricePerKg * amountUsed).toFixed(2);
       totalCost += parseFloat(costForIngredient);
-
-      // Print the cost for this ingredient to the console
-      console.log(`${ingredientName}: $${costForIngredient}`);
     }
 
-    // Get the selling price from the input element
     const sellingPrice = sellingPriceInput.value;
-
-    // Calculate the food cost in percentage
     const foodCostPercentage = ((totalCost / sellingPrice) * 100).toFixed(2);
-
-    // Set the food cost percentage as the text content of the result element
     resultElement.textContent = `${foodCostPercentage}%`;
 
-    // Set the background color of the result element based on the food cost percentage
+    // bg color according to the percentage of food cost
     switch (true) {
       case foodCostPercentage >= 35:
         resultElement.style.backgroundColor = "black";
